9c0606c047ac8e6dfaef752565e6046d840a2a61
darakhshanda <darakhshanda@hotmail.com>
2026-01-14 04:45:59 +0000

    Update url pattern for recipe.html and path updated to display recipe details in index.html

--- mealapp/urls.py
+++ mealapp/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('recipe/<int:recipe_id>/', views.recipe_detail, name='recipe_detail'),
     path('dashboard/', views.dashboard, name='dashboard'),
-    # path('signup/', views.signup, name='signup'),  
+    # path('signup/', views.signup, name='signup'),
     # path('login/', auth_views. LoginView.as_view(template_name='mealapp/login.html'), name='login'),
 ]
